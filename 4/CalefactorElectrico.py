5490fffd7057c64fc4bbf1d9bdd3adbf4cc50309
anto144 <104173968+anto144@users.noreply.github.com>
2022-06-06 03:45:24 -0300

    Add files via upload

---
+++ 4/CalefactorElectrico.py
@@ -0,0 +1,19 @@
+from Calefactor import calefactor
+
+class calefactorElectrico(calefactor):
+    __potenciaMaxima = None
+
+    def __init__(self,marca,modelo,potencia):
+        super().__init__(marca,modelo)
+        self.__potenciaMaxima = potencia
+    
+    def __str__(self):
+        cadena = ""
+        cadena += super().__str__() +("\nPotencia Maxima:{} watts".format(self.__potenciaMaxima))
+        return cadena 
+    
+    def getPotencia(self):
+        return self.__potenciaMaxima
+    
+    def __lt__(self,otro):
+        return self.__potenciaMaxima< otro.getPotencia()
